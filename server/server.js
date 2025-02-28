7534257471a16779381ec376174217934913c7ad
Drew-Andersen <andrewtandersen@gmail.com>
2025-02-28 08:11:16 -0500

    Created a simple server.js using elasticsearch, node.js, and express

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+// ElasticSearch imports
+const { Client } = require('@elastic/elasticsearch');
+const client = new Client({ node: 'http://localhost:9200' }); // change the address for Azure virtual machine
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+};
+
+// ElasticSearch error handling (example)
+client.ping({}, (error) => {
+    if (error) {
+        console.log('Elasticsearch cluster is down!');
+    } else {
+        console.log('Elasticsearch cluster is up and running!');        
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
